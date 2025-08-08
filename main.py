28ccff1810791fd4f3267bc51e55f9d33946ec08
adam <snowadam@icloud.com>
2025-08-08 11:04:00 +1000

    Reduce line breaks from 20 to 10

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import battle
 import utils
 
 print("Welcome to the turn-based battle simulator, inspired by the official Pokémon games!", end="")
-utils.line_break(20)
+utils.line_break(10)
 
 battle.battle_start()
